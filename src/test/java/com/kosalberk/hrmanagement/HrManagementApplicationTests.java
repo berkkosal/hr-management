5ed85a2d868c73217abbc5335d533b476ad80f15
berkkosal <kosalnberk@gmail.com>
2023-09-03 14:29:56 +0300

    first commit

---
+++ src/test/java/com/kosalberk/hrmanagement/HrManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosalberk.hrmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
