18ad7a7f0390cd74106a41c45e5f92f0f3f66894
berkkosal <kosalnberk@gmail.com>
2023-09-05 20:28:12 +0300

    AddEmployeeForm formu tamamlandı

--- frontend/src/containers/Employees/AddEmployeeForm.js
+++ frontend/src/containers/Employees/AddEmployeeForm.js
@@ -1,54 +1,58 @@
 import React from 'react'
 
 export default function AddEmployeeForm() {
+
+
     return (
         <>
-            <div className="form-row">
-
-                <div className="form-group col-md-6">
-                    <label for="inputName">Name</label>
-                    <input type="text" className="form-control" id="inputName" placeholder="Name" />
+            <form>
+                <div className="form-row">
+                    <div className="form-group col-sm-6">
+                        <label for="inputName">Name</label>
+                        <input type="text" className="form-control" id="inputName" placeholder="Name" />
+                    </div>
+                    <div className="form-group col-sm-6">
+                        <label for="inputSurname">Surname</label>
+                        <input type="text" className="form-control" id="inputSurname" placeholder="Surname" />
+                    </div>
                 </div>
+                <div className="form-row">
+                    <div className="form-group col-sm-4">
+                        <label for="inputEmail">E-mail</label>
+                        <input type="email" className="form-control" id="inputEmail" placeholder="E-mail" />
+                    </div>
 
-                <div className="form-group col-md-6">
-                    <label for="inputSurname">Surname</label>
-                    <input type="text" className="form-control" id="inputSurname" placeholder="Surname" />
+                    <div className="form-group col-sm-4">
+                        <label for="inputPhoneNumber">Phone</label>
+                        <input type="text" className="form-control" id="inputPhoneNumber" placeholder="Phone" />
+                    </div>
+
+                    <div className="form-group col-sm-4">
+                        <label for="inputPassword4">Password</label>
+                        <input type="password" className="form-control" id="inputPassword4" placeholder="Password" />
+                    </div>
+
                 </div>
-
-            </div>
-            <div className="form-row">
-
-                <div className="form-group col-md-4">
-                    <label for="inputEmail">E-mail</label>
-                    <input type="email" className="form-control" id="inputEmail" placeholder="E-mail" />
+                <div className="form-row">
+                    <div className="form-group col-sm-4">
+                        <label for="departmentName">Department</label>
+                        <select id="departmentName" class="custom-select" required>
+                            <option value="">Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div className="form-group col-sm-4">
+                        <label for="startingDate">Date of Start</label>
+                        <input className="form-control" type="date" id="startingDate"></input>
+                    </div>
+                    <div className="form-group col-sm-4">
+                        <label for="salary">Salary</label>
+                        <input type="text" id="salary" className="form-control"></input>
+                    </div>
                 </div>
-
-                <div className="form-group col-md-4">
-                    <label for="inputPhoneNumber">Phone</label>
-                    <input type="text" className="form-control" id="inputPhoneNumber" placeholder="Phone" />
-                </div>
-
-                <div className="form-group col-md-4">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" className="form-control" id="inputPassword4" placeholder="Password" />
-                </div>
-
-            </div>
-
-
-            <div className="form-row">
-
-                <div class="form-group col-md-6">
-                    <select class="custom-select" required>
-                        <option value="">Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
-
-            </div>
-
+            </form>
         </>
 
 
