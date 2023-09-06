e992f13c5aff45ffe95a0d346ed071e4140973de
berkkosal <kosalnberk@gmail.com>
2023-09-06 21:43:39 +0300

    Update EmployeeControllerImpl.java

--- src/main/java/com/kosalberk/hrmanagement/controller/impl/EmployeeControllerImpl.java
+++ src/main/java/com/kosalberk/hrmanagement/controller/impl/EmployeeControllerImpl.java
@@ -1,12 +1,15 @@
 package com.kosalberk.hrmanagement.controller.impl;
 
 import com.kosalberk.hrmanagement.controller.EmployeeController;
+import com.kosalberk.hrmanagement.model.dto.EmployeeDto;
 import com.kosalberk.hrmanagement.model.request.AddEmployeeRequest;
 import com.kosalberk.hrmanagement.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
@@ -15,9 +18,17 @@ public class EmployeeControllerImpl implements EmployeeController {
 
     private EmployeeService employeeService;
 
+    //*Generic olarak kullanamıyorum?
     @Override
-    public ResponseEntity addEmploye(AddEmployeeRequest request) {
+    public ResponseEntity<AddEmployeeRequest> addEmployee(AddEmployeeRequest request) {
         employeeService.register(request);
         return ResponseEntity.ok().body(request);
+    }
+
+
+    //*Generic olarak kullanamıyorum?
+    @Override
+    public ResponseEntity<EmployeeDto> getEmployee(UUID uuid) {
+       return ResponseEntity.ok().body(employeeService.getById(uuid));
     }
 }
