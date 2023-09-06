dd63c86b670d2f75cdd551b5210dc0ab2a07f0a6
berkkosal <kosalnberk@gmail.com>
2023-09-06 21:43:29 +0300

    getEmployee eklendi

--- src/main/java/com/kosalberk/hrmanagement/controller/EmployeeController.java
+++ src/main/java/com/kosalberk/hrmanagement/controller/EmployeeController.java
@@ -1,15 +1,21 @@
 package com.kosalberk.hrmanagement.controller;
 
+import com.kosalberk.hrmanagement.model.dto.EmployeeDto;
 import com.kosalberk.hrmanagement.model.request.AddEmployeeRequest;
-import com.kosalberk.hrmanagement.model.response.BaseResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.UUID;
 
 public interface EmployeeController {
 
     @PostMapping("/add-employee")
-    ResponseEntity<BaseResponse<?>> addEmploye(@RequestBody AddEmployeeRequest request);
+    ResponseEntity<AddEmployeeRequest> addEmployee(@RequestBody AddEmployeeRequest request);
 
+    @GetMapping("/get-employee/{uuid}")
+    ResponseEntity<EmployeeDto> getEmployee(@PathVariable UUID uuid);
 
 }
