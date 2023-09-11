13bea14f1c59fc62ca7a352fbcd53a9046b41752
berkkosal <kosalnberk@gmail.com>
2023-09-11 20:45:53 +0300

    utils, assets eklendi

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,9 +3,18 @@ import React from 'react'
 export default function Footer() {
   return (
     <div style={{
-      backgroundColor:"black",
-      
+      backgroundColor: "black",
+
     }}>
+
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        </div>
+        <img class="card-img-bottom" src="..."/>
+      </div>
       FOOTER
     </div>
   )
