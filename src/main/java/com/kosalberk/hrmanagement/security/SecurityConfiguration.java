0380b93491804ad68abc45587f9fd72cbacd1af9
berkkosal <kosalnberk@gmail.com>
2023-09-27 22:38:17 +0300

    security configuration oluşturuldu, SecurityFilterChain oluşturuldu

---
+++ src/main/java/com/kosalberk/hrmanagement/security/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.kosalberk.hrmanagement.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    // "/" olarak tek bir path'e izin veriliyor. /** gerekli.
+    // requestMatchers sıralamaları önemli. Bir request bir pattern'le eşleştikten sonra
+    // geriye kalan hiçbir patterne bakılmıyor.
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        (requests) -> requests
+                                .requestMatchers("/get-employees").authenticated()
+                                .requestMatchers("/**").permitAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
+
